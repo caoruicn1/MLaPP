b54a1c938b2a5f485e14aa39a9c3ae3e7f0f95dd
Taro Sato <okomestudio@gmail.com>
2014-03-06 12:40:45 -0800

    naive bayes

--- demos/naive_bayes_bow_demo.py
+++ demos/naive_bayes_bow_demo.py
@@ -4,65 +4,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pmtk3.data_tool import load_data
 from pmtk3.toolbox.supervised_model import naive_bayes
+from pmtk3.stats import mutual_info
 
 
 def main():
     data = load_data('XwindowsDocData')
     Xtrain = data['xtrain']
     ytrain = np.ravel(data['ytrain'])
+    vocab = [o[0] for o in data['vocab']]
 
+    # train a model
     model = naive_bayes.fit(Xtrain, ytrain)
-
     for idx, theta in enumerate(model.theta):
         plt.figure(idx)
         plt.plot(theta)
         plt.title('p(x_j=1|y={})'.format(idx+1))
 
-    plt.show()
+    #plt.show()
 
-    exit()
-
-    X = data['documents']
-    y = np.array(data['newsgroups'])[0]
-    classlabels = [o[0] for o in data['groupnames'][0]]
-    wordlist = np.array([o[0] for o in data['wordlist'][0]])
-
-    # pick the first 1000 by word count
-    nwords = np.array(X.sum(axis=0))[0]
-    ndx = nwords.argsort()[::-1]
-    ndx = ndx[:1000]
-    XX = X[:,ndx]
-    yy = y[ndx]
-
-    # sort by label
-    ndx = yy.argsort()
-    yy = yy[ndx]
-    XX = XX[:,ndx]
-
-    # plot
-    xx = np.transpose(XX.todense())
-    plt.imshow(xx, aspect='auto', interpolation='none')
-
-    for nrow in np.cumsum(np.bincount(yy)[1:]):
-        plt.axhline(nrow, lw=3)
-
-    plt.xlim(0, 99)
-    plt.ylim(999, 0)
-    plt.xlabel('words')
-    plt.ylabel('documents')
-    plt.show()
-
-    # randomly sample bag of words for each class
-    i = 0
-    for idx, j in enumerate(np.cumsum(np.bincount(yy)[1:])):
-        k = np.random.random_integers(i, j)
-        m = np.array(XX[:,k].toarray().flatten(), dtype=bool)
-
-        print classlabels[idx]
-        print ' '.join(wordlist[m])
+    # top n words
+    n = 5
+    for idx, theta in enumerate(model.theta):
+        ndx = theta.argsort()[::-1]
+        sorted_prob = theta[ndx]
+        print 'top {} words for class {}'.format(n, idx+1)
+        for w in range(0, n):
+            print '{:d} {:6.4f} {}'.format(w+1, sorted_prob[w], vocab[ndx[w]][0])
         print
 
-        i = j
+    # compute mutual information between words and class labels
+    mi = mutual_info.class_features_binary(Xtrain, ytrain)
+    ndx = mi.argsort(axis=0)[::-1]
+    sorted_mi = mi[ndx]
+    print 'top {} words sorted by mutual information'.format(n)
+    for w in range(0, n):
+        print '{} {:6.4f} {}'.format(w + 1, sorted_mi[w], vocab[ndx[w]][0])
+    print 
 
 
 if __name__ == '__main__':
---
+++ pmtk3/stats/mutual_info.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+import numpy as np
+from pmtk3.toolbox.supervised_model import naive_bayes
+
+
+def class_features_binary(X, y):
+    model = naive_bayes.fit(X, y)
+    py, z = model.class_prior
+    pxy = model.theta
+    C, D = pxy.shape
+
+    # px(j) = p(x=j) = sum_c p(xj=1|y=c) p(y=c)
+    px = (py[:, np.newaxis] * pxy).sum(axis=0)
+
+    mi = np.zeros(D)
+    for c in range(0, C):
+        mi += py[c] * (pxy[c,:] * np.log2(pxy[c,:] / px)
+                       + (1. - pxy[c,:]) * np.log2((1. - pxy[c,:]) / (1. - px)))
+
+    return mi
